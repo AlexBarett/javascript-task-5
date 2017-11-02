460ea316af385baa68d60856eba0cdd6d9f6b38a
AlexBarett <iron.angel876527@gmail.com>
2017-11-02 12:11:33 +0500

    первый блин комом

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -13,32 +13,65 @@ module.exports = getEmitter;
  */
 function getEmitter() {
     return {
+        subscriptions: {},
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (this.subscriptions[event]) {
+                this.subscriptions[event].push({ person: context, func: handler });
+            } else {
+                this.subscriptions[event] = [{ person: context, func: handler }];
+            }
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            var newSubscriptions = this.subscriptions;
+            if (typeof this.subscriptions[event] !== 'undefined') {
+                Object.keys(newSubscriptions).forEach(function (events) {
+                    if (events === event || events.includes(event + '.')) {
+                        newSubscriptions[events] = newSubscriptions[events]
+                            .filter(student => student.person !== context);
+                    }
+                });
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            if (this.subscriptions[event]) {
+                this.subscriptions[event].forEach(function (element) {
+                    element.func.call(element.person);
+                });
+            }
+            if (event.includes('.')) {
+                if (this.subscriptions[event.split('.')[0]]) {
+                    this.subscriptions[event.split('.')[0]].forEach(function (element) {
+                        element.func.call(element.person);
+                    });
+                }
+            }
+
+            return this;
         },
 
         /**
